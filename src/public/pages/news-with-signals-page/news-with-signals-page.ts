e1db99efcab1696b36610e3cd7c1cda152b50f7c
klabarre <klabarre@dawan.fr>
2025-11-12 16:58:24 +0100

    correction correction pagination actualités avec les signaux

--- src/public/pages/news-with-signals-page/news-with-signals-page.ts
+++ src/public/pages/news-with-signals-page/news-with-signals-page.ts
@@ -1,31 +1,53 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { NewsService } from '../../services/news/news-service';
 import { IResponseApi } from '../../interfaces/IResponseApi';
 import { INews } from '../../interfaces/INews';
 import { ConsigneNewsPagination } from "../../components/consigne-news-pagination/consigne-news-pagination";
+import { NewsList } from "../../components/news-list/news-list";
 
 @Component({
   selector: 'app-news-with-signals-page',
-  imports: [ConsigneNewsPagination],
+  imports: [ConsigneNewsPagination, NewsList],
   templateUrl: './news-with-signals-page.html',
   styleUrl: './news-with-signals-page.css',
 })
-export class NewsWithSignalsPage implements OnInit {
+export class NewsWithSignalsPage {
   private service = inject(NewsService)
 
-  intialLoad = { page: 1, perPage: 6 }
+  constructor() {
+    effect(() => {
+      // Notre effect se relance à chaque changement de la valeur du signal 'paginationManagement()'
+      //  car le signal est utilisé dans la méthode loadNews()
+
+      this.loadNews()
+    })
+  }
+
+  paginationManagement = signal({ page: 1, perPage: 6 })
 
   respApi = signal<IResponseApi<INews>>({} as IResponseApi<INews>)
 
-  ngOnInit(): void {
-    this.loadNews(this.intialLoad.page, this.intialLoad.perPage)
-  }
-
-  loadNews(page: number, perPage: number) {
-    this.service.getPaginateNews(page, perPage).subscribe({
+  loadNews() {
+    this.service.getPaginateNews(this.paginationManagement().page, this.paginationManagement().perPage).subscribe({
       next: (resp) => this.respApi.set(resp),
       error: (e) => console.error(e)
     })
+  }
+
+  handlePrev() {
+    if (this.respApi().prev) {
+      this.paginationManagement.update((prev) => ({ ...prev, page: this.respApi().prev! }))
+    }
+  }
+
+  handleNext() {
+    if (this.respApi().next) {
+      this.paginationManagement.update((prev) => ({ ...prev, page: this.respApi().next! }))
+    }
+  }
+
+  get pageNbr() {
+    return this.respApi().prev ? this.respApi().prev! + 1 : 1
   }
 
 }
